720b4ee49e7129d69828e0d7d7b4bfb2874ee6eb
Vladimir Panteleev <git@thecybershadow.net>
2020-07-02 06:46:36 +0000

    t/t-2_dedup-5_free: New test

---
+++ t/t-2_dedup-5_free.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source common.bash
+
+# Test that thincow reuses COW block storage.
+
+dd if=/dev/zero of=upstream/test bs=64K seek=2 count=0 status=none
+run_thincow
+dd if=target/test of=/dev/null bs=64K status=none # populate hash table
+
+# First block
+dd if=/dev/urandom of=target/test bs=64K count=1 status=none conv=notrunc
+dd if=/dev/zero    of=target/test bs=64K count=1 status=none conv=notrunc
+
+# Second block
+dd if=/dev/urandom of=target/test bs=64K count=1 status=none conv=notrunc seek=1
+dd if=/dev/zero    of=target/test bs=64K count=1 status=none conv=notrunc seek=1
+
+fusermount -u target
+diff -u <(get_usage data/cowdata) /dev/stdin <<< $((4096 + 65536))
