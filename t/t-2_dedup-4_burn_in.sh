e22aba35941f1072b115610111e43af7eecd84be
Vladimir Panteleev <git@thecybershadow.net>
2020-07-02 06:42:50 +0000

    t/t-2_dedup-4_burn_in: New test

---
+++ t/t-2_dedup-4_burn_in.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source common.bash
+
+# Test that thincow consumes a constant amount of disk space
+# when the same block is written to repeatedly.
+
+dd if=/dev/zero of=upstream/test bs=64K seek=1 count=0 status=none
+run_thincow
+for _ in $(seq 100)
+do
+	dd if=/dev/urandom of=target/test bs=64K count=1 status=none conv=notrunc
+done
+fusermount -u target
+diff -u <(get_usage data/cowdata) /dev/stdin <<< $((4096 + 65536))
