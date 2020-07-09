14c52326562c2c555e2730d7129a4b16ee2210b4
Vladimir Panteleev <git@thecybershadow.net>
2020-07-09 02:32:57 +0000

    t/t-1_basic-5_partial: New test

---
+++ t/t-1_basic-5_partial.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source common.bash
+
+# Test partial trailing blocks.
+
+bytes=$((block_size * 3 / 2))
+
+( seq 1e99 || true ) | dd iflag=fullblock of=upstream/test bs=$bytes count=1 status=none
+run_thincow
+diff -q target/test upstream/test
+
+( seq 2 1e99 || true ) | dd iflag=fullblock of=target/test bs=$bytes count=1 status=none conv=notrunc
+diff -q target/test <( seq 2 1e99 | head -c "$bytes" )
+
+fusermount -u target
