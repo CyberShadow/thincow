190344d2624a6ebb7353b1b924583294f70d5a50
Vladimir Panteleev <git@thecybershadow.net>
2020-07-09 06:24:02 +0000

    t/t-2_dedup-5_free: Test writing distinct data
    Fix code to match the intent of the test.

--- t/t-2_dedup-5_free.sh
+++ t/t-2_dedup-5_free.sh
@@ -8,12 +8,12 @@ run_thincow
 dd if=target/test of=/dev/null bs=$block_size status=none # populate hash table
 
 # First block
-( seq 1e99 || true ) | dd iflag=fullblock of=target/test bs=$block_size count=1 status=none conv=notrunc
-dd if=/dev/zero                           of=target/test bs=$block_size count=1 status=none conv=notrunc
+( seq 1 1e99 || true ) | dd iflag=fullblock of=target/test bs=$block_size count=1 status=none conv=notrunc
+dd if=/dev/zero                             of=target/test bs=$block_size count=1 status=none conv=notrunc
 
 # Second block
-( seq 1e99 || true ) | dd iflag=fullblock of=target/test bs=$block_size count=1 status=none conv=notrunc seek=1
-dd if=/dev/zero                           of=target/test bs=$block_size count=1 status=none conv=notrunc seek=1
+( seq 2 1e99 || true ) | dd iflag=fullblock of=target/test bs=$block_size count=1 status=none conv=notrunc seek=1
+dd if=/dev/zero                             of=target/test bs=$block_size count=1 status=none conv=notrunc seek=1
 
 stop_thincow
 diff -u <(get_usage data/cowdata) /dev/stdin <<< $((1 * block_size))
