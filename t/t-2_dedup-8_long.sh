3c86817be998a22350cd6d2f65a4923656481a05
Vladimir Panteleev <git@thecybershadow.net>
2020-08-01 08:39:43 +0000

    t/t-2_dedup-8_long: New test

---
+++ t/t-2_dedup-8_long.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source common.bash
+
+# Test retroactive deduplication of data that thincow forgot about.
+
+block_count=$((hash_table_size * 3 / 2 / 8)) # 150% hash table capacity
+
+( seq 1e99 || true ) | dd iflag=fullblock of=upstream/test bs=$block_size count=$block_count status=none
+run_thincow
+dd if=target/devs/test of=/dev/null bs=$block_size status=none # populate hash table
+dd if=/dev/zero of=target/devs/test bs=$block_size status=none count=$block_count conv=notrunc # wipe
+stop_thincow
+run_thincow
+dd if=upstream/test of=target/devs/test bs=$block_size status=none conv=notrunc # deduplicate?
+stop_thincow
